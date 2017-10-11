3a3fdfdbe1d380efd27ca0ced45afbf071fa90e3
joecool2424 <joep24@gmail.com>
2017-10-11 18:01:29 -0500

    added azure interface

---
+++ HeartBeat/Models/AzureHeartBeat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace HeartBeat.Models
+{
+    public class AzureHeartBeat
+    {
+        public int HeartBeatRate
+        {
+            get;
+            set;
+        }
+
+        public bool HeartIsFailing
+        {
+            get;
+            set;
+        }
+    }
+}
--- HeartBeat/Services/AzureService.cs
+++ HeartBeat/Services/AzureService.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using System.Threading.Tasks;
+using HeartBeat.Models;
 namespace HeartBeat.Services
 {
-    public class AzureService
+    public interface IAzureService
+    {
+        Task<bool> SaveHeartBeat(AzureHeartBeat request);
+    }
+    public class AzureService : IAzureService
     {
         public AzureService()
         {
+        }
+
+        /// <summary>
+        /// Saves the heart beat.
+        /// </summary>
+        /// <returns>true, if operation succeeded. false, if operation failed.</returns>
+        /// <param name="request">Request is the heartbeat</param>
+        public async Task<bool> SaveHeartBeat(AzureHeartBeat request)
+        {
+            return await Task.FromResult(true);
         }
     }
 }
